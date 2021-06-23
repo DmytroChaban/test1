20390daec854b6cea8c9e6878124af1fe3a42312
Dmyytro Chaban <dmitriychaban95@gmail.com>
2021-06-23 18:22:26 +0300

    zapolnili count i  movies

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 " use strict";
+let NumberOfFilms = +prompt('How many films have you ever seen?',' ');
+let personalMovieDB ={
+     count:NumberOfFilms,
+     movies:{},
+     actors:{},
+     genres:[],
+     privat:false
+}; 
+let lastFilm_1 = prompt('ожин из последних','');
+let rating_1 = prompt('рейтинг','');
+let lastFilm_2 = prompt('ожин из последних','');
+let rating_2 = prompt('рейтинг','');
+personalMovieDB.movies[lastFilm_1]= rating_1;
+personalMovieDB.movies[lastFilm_2]= rating_2;     
+   
 
-let  uuu = false;
-     www = true;
-console.log(uuu && www);
-console.log(5**3);
-alert('eee');
+console.log(personalMovieDB);
