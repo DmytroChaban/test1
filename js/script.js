90b7f4be07b707b1d81092688bbd5961cc130951
Dmyytro Chaban <dmitriychaban95@gmail.com>
2021-06-25 12:29:39 +0300

    practica2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 " use strict";
 let NumberOfFilms = +prompt('How many films have you ever seen?',' ');
+
 let personalMovieDB ={
      count:NumberOfFilms,
      movies:{},
@@ -7,12 +8,37 @@ let personalMovieDB ={
      genres:[],
      privat:false
 }; 
-let lastFilm_1 = prompt('ожин из последних','');
-let rating_1 = prompt('рейтинг','');
-let lastFilm_2 = prompt('ожин из последних','');
-let rating_2 = prompt('рейтинг','');
-personalMovieDB.movies[lastFilm_1]= rating_1;
-personalMovieDB.movies[lastFilm_2]= rating_2;     
+
+if (personalMovieDB.count< 10){
+     alert('вы новичок');
+} else if (personalMovieDB.count<30){
+     alert('вы любитель');
+} else if (personalMovieDB.count>30) {
+     alert('вы киноман');
+}else {
+     alert('произошла ошибка');
+}
+
+for(let i = 1;i<3;i++) {
+      let lastFilm = prompt('один из последних','');  
+
+     while(true) { 
+          if (lastFilm == '' || lastFilm == null || lastFilm.length>50) {
+               lastFilm = prompt('Название фильма должно состоять хотя-бы из 1 символа и не больше 50','');              
+          } else {break;}
+     };
+
+     let rating = prompt('рейтинг','');
+     
+     while(true){ 
+           if (rating == '' || rating>10 || rating<1) {
+                   rating = +prompt('рейтинг должен быть от 1 до 10','');              
+          } else {break;}
+     };
+     personalMovieDB.movies[lastFilm] = rating;
+};
+
+    
    
 
 console.log(personalMovieDB);
