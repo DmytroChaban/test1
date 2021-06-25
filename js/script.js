504c37f7beaf2ed7ffd92f0a8acf9465f2246f1e
Andriy <chaban.andriy82@gmail.com>
2021-06-25 12:27:34 +0300

    test commit

--- js/script.js
+++ js/script.js
@@ -7,12 +7,20 @@ let personalMovieDB ={
      genres:[],
      privat:false
 }; 
-let lastFilm_1 = prompt('ожин из последних','');
-let rating_1 = prompt('рейтинг','');
-let lastFilm_2 = prompt('ожин из последних','');
-let rating_2 = prompt('рейтинг','');
-personalMovieDB.movies[lastFilm_1]= rating_1;
-personalMovieDB.movies[lastFilm_2]= rating_2;     
-   
+// let lastFilm_1 = prompt('один из последних','');
+// let rating_1 = prompt('рейтинг','');
+// let lastFilm_2 = prompt('один из последних','');
+// let rating_2 = prompt('рейтинг','');
+// personalMovieDB.movies[lastFilm_1]= rating_1;
+// personalMovieDB.movies[lastFilm_2]= rating_2;     
+
+for (let i=1; i<3; i++) {
+     let lastFilm = prompt('Один из последних','');     
+     let rating = prompt('рейтинг','');
+     if (lastFilm == '' || lastFilm.length>50 || lastFilm==null || rating==null || rating == '' || rating.length>50) {
+          i--; continue;
+     };
+     personalMovieDB.movies[lastFilm]= rating;
+}
 
 console.log(personalMovieDB);
